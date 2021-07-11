fbb81d8fe561466ee0bd8e3f1fc3608ac07aeb1a
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-07-11 00:09:51 -0300

    created item_card component

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -1,7 +1,9 @@
 import 'package:e_commerce_bags/constants.dart';
+import 'package:e_commerce_bags/models/Product.dart';
 import 'package:flutter/material.dart';
 
 import 'categories.dart';
+import 'item_card.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -20,6 +22,23 @@ class Body extends StatelessWidget {
           ),
         ),
         Categories(),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            child: GridView.builder(
+              itemCount: products.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: kDefaultPadding,
+                crossAxisSpacing: kDefaultPadding,
+                childAspectRatio: 0.75,
+              ),
+              itemBuilder: (context, index) => ItemCard(
+                product: products[index],
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
