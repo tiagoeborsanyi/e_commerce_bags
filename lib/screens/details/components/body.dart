39c4bdad423d1557f9a4247fdc167b7a73083e73
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-07-11 20:51:52 -0300

    finish first part in details

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,6 +1,13 @@
+import 'package:e_commerce_bags/constants.dart';
 import 'package:e_commerce_bags/models/Product.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
+import 'add_to_cart.dart';
+import 'cart_counter.dart';
+import 'color_and_size.dart';
+import 'counter_with_fav_btn.dart';
+import 'description.dart';
 import 'product_title_with_image.dart';
 
 class Body extends StatelessWidget {
@@ -20,14 +27,30 @@ class Body extends StatelessWidget {
             child: Stack(
               children: [
                 Container(
-                  margin: EdgeInsets.only(top: size.height * 0.35),
-                  height: 450,
+                  margin: EdgeInsets.only(top: size.height * 0.37),
+                  padding: EdgeInsets.only(
+                    top: size.height * 0.12,
+                    left: kDefaultPadding,
+                    right: kDefaultPadding,
+                  ),
+                  // height: 450,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(24),
                       topRight: Radius.circular(24),
                     ),
+                  ),
+                  child: Column(
+                    children: [
+                      ColorAndSize(product: product),
+                      SizedBox(height: kDefaultPadding / 2),
+                      Description(product: product),
+                      SizedBox(height: kDefaultPadding / 2),
+                      CounterWithFavBtn(),
+                      SizedBox(height: kDefaultPadding / 2),
+                      AddToCart(product: product),
+                    ],
                   ),
                 ),
                 ProductTitleWithImage(product: product)
