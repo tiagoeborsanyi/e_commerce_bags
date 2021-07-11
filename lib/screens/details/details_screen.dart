afd5db3124a3db2f6b8894ac2e386959e1de0501
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-07-11 18:57:40 -0300

    create appBar to details

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -1,5 +1,8 @@
+import 'package:e_commerce_bags/constants.dart';
 import 'package:e_commerce_bags/models/Product.dart';
+import 'package:e_commerce_bags/screens/details/components/body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DetailScreen extends StatelessWidget {
   final Product product;
@@ -8,6 +11,38 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      // each product have a color
+      backgroundColor: product.color,
+      appBar: buildAppBar(context),
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: product.color,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset(
+          'assets/icons/back.svg',
+          color: Colors.white,
+        ),
+        onPressed: () => Navigator.pop(context),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset('assets/icons/search.svg'),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset('assets/icons/cart.svg'),
+        ),
+        SizedBox(
+          width: kDefaultPadding / 2,
+        ),
+      ],
+    );
   }
 }
